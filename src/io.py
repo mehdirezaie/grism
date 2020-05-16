bb852391546801cfbcbc064d9c1b9ecaf00d6d7f
Mehdi Rezaie <medirz90@icloud.com>
2020-05-15 20:19:28 -0400

    no info

--- src/io.py
+++ src/io.py
@@ -10,56 +10,56 @@ from astropy.io import fits
 
 
 class array(numpy.ndarray):
-    ''' Wrapper around numpy.ndarray to facilitate visualization of 2D images 
+    ''' Wrapper around numpy.ndarray to facilitate visualization of 2D images
     using imshow
     '''
     def __new__(cls, a):
         obj = numpy.asarray(a).view(cls)
         return obj
-    
-    def imshow(self, ax=None, vmin=0, vmax=0.2, origin='lower', 
+
+    def imshow(self, ax=None, vmin=0, vmax=0.2, origin='lower',
                cmap=plt.cm.binary, title='', hold=False, **kwargs):
         if ax is None:
             fig, ax = plt.subplots()
-            
+
         map1 = ax.imshow(self, vmin=vmin, vmax=vmax, origin=origin, cmap=cmap, **kwargs)
         ax.set_title(title)
-        
+
         if not hold:
             plt.show()
 
 class DataLoader:
     '''
-    DataLoader    
-    
+    DataLoader
+
     '''
-    
+
     def __init__(self, science_img, header_ext=0):
         '''
-        
+
         inputs
         -------
         science_img: str, path to the science image
         header_ext: int, extension number of the main header
-        
+
         '''
-        
+
         self.data = fits.open(science_img)
         self.data.info()
-            
+
         self.num_ext = len(self.data)
-        
+
     def read_ext(self, ext=1):
-        ''' 
-        
+        '''
+
         inputs
         --------
-        ext: int, extension number 
-        
+        ext: int, extension number
+
         '''
         assert ext < self.num_ext, f'ext={ext} does not exist'
         return array(self.data[ext].data) # this will add method 'imshow'
-    
+
     def get_keyword(self, keyword, ext=0):
         '''
         inputs
@@ -67,6 +67,3 @@ class DataLoader:
         keyword: str
         '''
         return self.data[ext].header.get(keyword)
-    
-    def __repr__(self):
-        return self.info
